cba4803a8dde88cabfb381e32f65d866243d215b
BvdBilt <vandebilt@stormdigital.nl>
2019-04-26 16:12:07 +0200

    added funtionality to change brightness

--- gui_project/test.py
+++ gui_project/test.py
@@ -3,28 +3,19 @@ from PIL import Image, ImageTk
 from tkinter import filedialog
 window = Tk()
 
-window.title("Image selectah")
+window.title("Image selector")
 
 window.geometry('600x600')
 
-path = filedialog.askopenfilename(filetypes=[("Image File",'.jpg')])
-im = Image.open(path)
-tkimage = ImageTk.PhotoImage(im)
-showImg=Label(window,image = tkimage)
-showImg.image = tkimage
-showImg.pack()
+def select_image():
+    path = filedialog.askopenfilename(filetypes=[("Image File",'.jpg')])
+    im = Image.open(path)
+    tkimage = ImageTk.PhotoImage(im)
+    showImg=Label(window, image=tkimage)
+    showImg.image = tkimage
+    showImg.pack()
 
-# def select_image(self):
-#
-#     path = filedialog.askopenfilename(filetypes=[("Image File",'.jpg')])
-#     im = Image.open(path)
-#     tkimage = ImageTk.PhotoImage(im)
-#     showImg=Label(window,image = tkimage)
-#     showImg.image = tkimage
-#     showImg.pack()
-#     return self
-
-# btn = Button(window, text="Select image", command=select_image)
-# btn.pack(side="bottom", fill="both", expand="yes", padx="100", pady="100")
+btn = Button(window, text="Select image", command=select_image)
+btn.pack(side="bottom", fill="both", expand="yes", padx="100", pady="100")
 
 window.mainloop()
